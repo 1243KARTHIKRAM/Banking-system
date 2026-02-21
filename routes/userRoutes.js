0eef05fb63dde6bd91d96c1c66120b1c94006e76
ram <1243gkarthik@gmail.com>
2026-02-21 21:55:44 +0530

    phase1

---
+++ routes/userRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+// User routes will be defined here
+// Example: router.get('/profile', controller.getProfile);
+
+module.exports = router;
